80ffc929311d7749f49efb49167a8dbbb3a52686
simja <simja@example.com>
2023-05-08 15:42:48 -0600

    First commitment to the general structure of the project

---
+++ README.md
@@ -0,0 +1,1 @@
+# lorenzas-coffee-api
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const morgan = require('morgan')
+
+// Initializations
+const app = express()
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+
+// Middlewares
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/login', require('./routes/LogIn.routes'));
+
+
+// Server is listening 
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'))
+})
