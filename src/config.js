e0b96758f676edfcca2e9d54d0d7ee468a0a40b5
simjaMalkeinu <yorkam_malkeinu@outlook.es>
2023-06-01 14:20:04 -0600

    configuration enviroment of production variables

--- src/config.js
+++ src/config.js
@@ -6,6 +6,11 @@ export const PORT = process.env.PORT || 3000
 
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-export const DB_DATABASE = process.env.DB_DATABSE || 'lorenzascoffee'
+export const DB_DATABASE = process.env.DB_DATABASE || 'lorenzascoffee'
 export const DB_PORT = process.env.DB_PORT || 3306
 export const DB_HOST = process.env.DB_HOST || 'localhost'
+
+
+export const DATABASE_URL = process.env.DATABASE_URL || 'mysql://kl5nq2r2bn15qxw4caoz:pscale_pw_xOsJwHxsnK6v7rrpatrBpbjaVzjKn4iqdPGtfK4yhKW@aws.connect.psdb.cloud/lorenzascoffee?ssl={"rejectUnauthorized":true}'
+
+
--- src/db.js
+++ src/db.js
@@ -1,17 +1,19 @@
 import { createPool } from "mysql2/promise";
 
-import {
-    DB_DATABASE,
-    DB_HOST,
-    DB_PASSWORD,
-    DB_PORT,
-    DB_USER,
-  } from "./config.js";
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER, DB_PORT } from "./config.js";
 
 export const poll = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  port: DB_PORT,
   database: DB_DATABASE,
+  port: DB_PORT
 });
+
+// console.log("db is connected");
+
+// return conn;
+
+//console.log(DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER);
+
+//export const poll = mysql.createConnection(DATABASE_URL)
